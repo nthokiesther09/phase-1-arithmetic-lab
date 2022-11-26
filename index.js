00988ae31f992f583d478698fdc998028fab7fc3
nthokiesther09 <nthokiesther09@gmail.com>
2022-11-26 16:58:27 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+let multiply;
+let num1= 2;
+let num2 = 31;
+multiply = num1 * num2;
+console.log(multiply)
 
-// Write your code here
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let mod;
+let num3 =19
+let num4 =5
+mod = num3 % num4
+console.log(mod)
+
+
+
+let max = Math.max(3,4,5,8,15,20)
+console.log(max)
+
+
+
+   
+
+
+
+
